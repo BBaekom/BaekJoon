a4b767277aab0de5f9316e4477b557ec1d066b02
Pig <min208406@naver.com>
2024-01-09 15:13:16 +0900

    알고리즘 수업 - 알고리즘의 수행 시간 1

---
+++ 24262.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i;
+    cin >> i;
+    cout << 1 << endl
+         << 0 << endl;
+}
+
+// MenOfPassion(A[], n) {
+//     i = ⌊n / 2⌋;
+//     return A[i]; # 코드1
+// }
+// 코드1 의 수행 횟수는 1이고, 다항식으로 나타내면 O(1)이기 때문에 0차이다,

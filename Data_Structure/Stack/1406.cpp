651f24783f1d6011fb50bc7a63e5b067770e48a8
Pig <min208406@naver.com>
2024-03-06 23:49:20 +0900

    1406

---
+++ Data_Structure/Stack/1406.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string sentence;
+    int M;
+    cin >> sentence;
+
+    cin >> M; // 명령어 수
+
+    cout << sentence << endl;
+
+    return 0;
+}
